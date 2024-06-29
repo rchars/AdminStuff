2e23e0e2a787a78a5a02b410a0a66cd53c9f3d33
rchars <rchars@protonmail.com>
2024-06-29 14:45:31 +0200

    fix autologin

--- autologin.bat
+++ autologin.bat
@@ -1,14 +1,25 @@
 @echo off
 
+echo Setting admin account
 net user admin admin /add
 net localgroup Administrators admin /add
+echo Setting admin password
+net user admin *
 
+echo Setting worker account
 net localgroup Administrators worker /delete
 net localgroup Users worker /add
 
+echo Setting auto-logon for worker
 reg add "HKLM\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon" /v DefaultUserName /d worker /f
-reg add "HKLM\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon" /v DefaultPassword /d "" /f
+reg add "HKLM\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon" /v DefaultPassword /d "worker" /f
 reg add "HKLM\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon" /v AutoAdminLogon /d 1 /f
+
+echo Setting password never expires for admin
+wmic UserAccount where Name='admin' set PasswordExpires=false
+
+echo Setting password never expires for worker
+wmic UserAccount where Name='worker' set PasswordExpires=false
 
 echo All done!
 pause
